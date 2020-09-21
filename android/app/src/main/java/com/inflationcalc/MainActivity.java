04a526537617a5f123fe5a5af3863a837f788827
leobur <leonardo.burrone@matera.com>
2020-09-21 16:48:27 -0300

    Rename app

--- android/app/src/main/java/com/inflationcalc/MainActivity.java
+++ android/app/src/main/java/com/inflationcalc/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rnbaseproject;
+package com.inflationcalc;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "RNBaseProject";
+    return "InflationCalc";
   }
 }
--- android/app/src/main/java/com/inflationcalc/MainApplication.java
+++ android/app/src/main/java/com/inflationcalc/MainApplication.java
@@ -1,4 +1,4 @@
-package com.rnbaseproject;
+package com.inflationcalc;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.rnbaseproject.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.inflationcalc.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
