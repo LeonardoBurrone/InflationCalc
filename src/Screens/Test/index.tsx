ec014b4c96368e8799c93bc3ffcc9c3135a62893
leobur <leonardo.burrone@matera.com>
2020-09-22 19:42:37 -0300

    Add appCenter crash check and track event with appCenter analytics

--- src/Screens/Test/index.tsx
+++ src/Screens/Test/index.tsx
@@ -1,12 +1,34 @@
+import Analytics from 'appcenter-analytics';
+import Crashes from 'appcenter-crashes';
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Alert, Button, Text, View } from 'react-native';
 
 import Styles from './styles';
 
 const Test: React.FunctionComponent = () => {
+  React.useEffect(() => {
+    checkForRecentCrashes();
+  }, []);
+
+  const checkForRecentCrashes = async (): Promise<void> => {
+    const didCrash: boolean = await Crashes.hasCrashedInLastSession();
+    if (didCrash) {
+      Alert.alert('', 'Sorry about that crash, we are working on a solution');
+    }
+  };
+
   return (
     <View>
       <Text>Initial Screen</Text>
+      <Button
+        title='Calculate inflation'
+        onPress={() =>
+          Analytics.trackEvent('calculate_inflation', {
+            Property1: 'Test1',
+            Property2: 'Test2',
+          })
+        }
+      />
     </View>
   );
 };
